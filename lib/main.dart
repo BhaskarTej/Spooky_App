e9acdd9b958d0330bc96bab866b3c328ef623f5c
BhaskarTej <bhaskarteja02@gmail.com>
2024-10-08 19:08:47 -0400

    Second Init: background Noise

--- lib/main.dart
+++ lib/main.dart
@@ -30,4 +30,39 @@ class HalloweenGame extends StatefulWidget {
   @override
   State<HalloweenGame> createState() => _HalloweenGameState();
 }
+class _HalloweenGameState extends State<HalloweenGame> {
+  // Audio players for background music and sound effects
+  late AudioPlayer _backgroundPlayer;
+  late AudioPlayer _effectPlayer;
+
+  // Random positions for spooky items
+  Offset _ghostPosition = Offset(100, 100);
+  Offset _pumpkinPosition = Offset(200, 150);
+  Offset _batPosition = Offset(50, 300);
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Initialize audio players
+    _backgroundPlayer = AudioPlayer();
+    _effectPlayer = AudioPlayer();
+
+    // Load and start playing background music in a loop
+    _backgroundPlayer.setLoopMode(LoopMode.all);
+    _backgroundPlayer.setAsset('assets/sounds/background_music.mp3').then(
+          (_) => _backgroundPlayer.play(),
+        );
+
+    // Randomize positions every few seconds
+    Timer.periodic(const Duration(seconds: 3), (timer) => _randomizePositions());
+  }
+
+  @override
+  void dispose() {
+    // Dispose audio players when not in use
+    _backgroundPlayer.dispose();
+    _effectPlayer.dispose();
+    super.dispose();
+  }
 
